d5d2bddcbdd64cdca9b2055fa8d0cea682b1f7f0
Barnabas Bulpett <bbulpett@charter.net>
2016-05-02 22:27:27 -0400

    Add Invitation model and newInvitation action to index controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -11,9 +11,18 @@ export default Ember.Controller.extend({
 	actions: {
 
 		saveInvitation() {
-			alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
-			this.set("responseMessage", `Thank you! We just saved your email address: ${this.get('emailAddress')}`);
+
+			const email = this.get('emailAddress');
+
+			const newInvitation = this.store.createRecord('invitation', { email: email });
+			newInvitation.save();
+
+			this.set('responseMessage', `Thank you! We just saved your email address: ${this.get('emailAddress')}`);
 			this.set('emailAddress', '');
+
+			// alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+			// this.set("responseMessage", `Thank you! We just saved your email address: ${this.get('emailAddress')}`);
+			// this.set('emailAddress', '');
 		}
 	}
 
